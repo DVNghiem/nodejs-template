2c1a1db37ceb14359c1c98b0d1406006129ae323
Dang Van Nghiem <vannghiem848@gmail.com>
2024-06-02 16:39:51 +0700

    feat: conifg husky (main)

--- env/index.js
+++ env/index.js
@@ -1,3 +1,3 @@
-const dotenv = require ('dotenv')
+const dotenv = require('dotenv');
 var path = require('path');
-dotenv.config({path:`${path.resolve(__dirname)}/${process.env.NODE_ENV || 'local'}.env`})
+dotenv.config({ path: `${path.resolve(__dirname)}/${process.env.NODE_ENV || 'local'}.env` });
